c8b2f611ad03bb87e5631ed316e156ea2fd9ae33
Adel Harrat <a03l.web@gmail.com>
2024-11-15 15:58:05 +0100

    Style: add mobile drawer & responsify header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,14 +1,23 @@
 <script>
 	import { Button } from '@/ui/button';
 	import { PUBLIC_WEBSITE_TITLE } from '$env/static/public';
+	import MobileDrawer from './MobileDrawer.svelte';
 </script>
 
 <header class="flex items-center justify-between gap-8">
 	<a
 		href="/"
-		class="flex items-center gap-4 text-xl font-extrabold tracking-tight hover:underline hover:decoration-orange-500 hover:decoration-2 hover:underline-offset-4"
+		class="flex items-center gap-4 text-base font-extrabold tracking-tight hover:underline hover:decoration-orange-500 hover:decoration-2 hover:underline-offset-4 md:text-xl xl:text-2xl"
 	>
-		<img src="/koala.png" alt="koala happy face" class="size-10" />
+		<img src="/koala.png" alt="koala happy face" class="size-7 md:size-10 xl:size-14" />
 		<span>{PUBLIC_WEBSITE_TITLE}</span>
 	</a>
+
+	<div class="md:hidden">
+		<MobileDrawer />
+	</div>
+
+	<div class="hidden md:block">
+		<!-- TODO: Add user avatar and dropdown, or add a login button -->
+	</div>
 </header>
---
+++ src/lib/components/MobileDrawer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import * as Drawer from '$lib/components/ui/drawer/index.js';
+	import { Button } from '@/ui/button';
+</script>
+
+<Drawer.Root>
+	<!-- TODO: Add menu bars icon -->
+	<Drawer.Trigger>OPEN</Drawer.Trigger>
+	<Drawer.Content>
+		<Drawer.Header>
+			<Drawer.Title>Are you sure absolutely sure?</Drawer.Title>
+			<Drawer.Description>This action cannot be undone.</Drawer.Description>
+		</Drawer.Header>
+		<Drawer.Footer>
+			<Button>Submit</Button>
+			<Drawer.Close>Cancel</Drawer.Close>
+		</Drawer.Footer>
+	</Drawer.Content>
+</Drawer.Root>
